f8d99b1eab5b402ca5a0f21d433304411613439e
huangruitian <576536299@qq.com>
2018-04-30 19:27:52 +0800

    Update homework.js

--- homework.js
+++ homework.js
@@ -18,6 +18,16 @@
             }
             return lastStr; 
         }
+        //首次出现的字符。
+        function unique(str){
+            for(var i = 0; i < str.length; i++){
+                //str.indexOf(str[i],i+1) === -1 确保后面没有。（后面都没有返回-1）
+                //str.indexOf(str[i]) === i 返回第一个出现的位置。
+                if(str.indexOf(str[i],i+1) === -1 && (str.indexOf(str[i]) === i)){
+                    return str[i];
+                }
+            }
+        }
         //经典的闭包
         // var liArr = document.getElementsByTagName('li');
         // var len = liArr.length;
